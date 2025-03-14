b52f7636b16b80300804a51f7af3ee7bc87c1af6
duongcuong4395 <dangduongcuog@gmail.com>
2025-03-14 09:33:19 +0700

    Init

---
+++ MyShop/MyShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyShop
+//
+//  Created by Macbook on 14/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyShop/MyShop/MyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShopApp.swift
+//  MyShop
+//
+//  Created by Macbook on 14/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
